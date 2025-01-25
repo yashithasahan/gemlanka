5e1bdb9b6e50d6ec764ae73f4b9344dd66849a4d
yashithasahan <yashithasahan@gmail.com>
2025-01-25 01:12:31 +0000

    refac : home page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`bg-white ${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`bg-white ${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         <Header />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col p-2 gap-4">
+    <div className="h-full flex flex-col p-2 gap-4 mx-auto max-w-[1200px]">
       <Carousel />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+      <div className="grid grid-cols-1 md:grid-cols-2  gap-4 ">
         <ItemCard
           imageUrl="https://chariot.tours/wp-content/uploads/2017/11/assorted-coloured-gemstones-1-e1691055918337.jpg"
           title="Sample Item"
@@ -72,8 +72,8 @@ export default function Home() {
           location="Colombo, Sri Lanka"
           dateTime="2023-10-01 10:00 AM"
         />
-        <Pagination />
       </div>
+      <Pagination />
     </div>
   );
 }
